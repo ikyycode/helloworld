c1f58f075d875dfbebe503b28f54e8017241cee1
rizkijanuarr <72480809+rizkijanuarr@users.noreply.github.com>
2022-06-09 13:03:57 +0700

    silicing update

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container">
+        <a class="navbar-brand" href="{{ route('welcome') }}">
+          <img src="{{asset('images/logo.png')}}" alt="" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#"
+                >Landing</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Programs</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Community</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Stackoverflow</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+          </ul>
+          <div class="d-flex">
+            <a href="{{ route('login') }}" class="btn btn-master btn-secondary me-3"> Sign In </a>
+            <a href="{{ route('login') }}" class="btn btn-master btn-primary"> Sign Up </a>
+          </div>
+        </div>
+      </div>
+    </nav>
