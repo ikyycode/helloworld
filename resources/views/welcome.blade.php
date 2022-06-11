1a9f70e9aef384718216f0482e7ae93a48c85861
rizkijanuarr <72480809+rizkijanuarr@users.noreply.github.com>
2022-06-12 04:54:58 +0700

    baru

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -225,7 +225,7 @@
                     <div class="clear"></div>
                   </div>
                   <p>
-                    <a href="{{ route('checkout') }}" class="btn btn-master btn-primary w-100 mt-3">
+                    <a href="{{ route('checkout', 'gila-belajar') }}" class="btn btn-master btn-primary w-100 mt-3">
                       Take This Plan
                     </a>
                   </p>
@@ -259,7 +259,7 @@
                     <div class="clear"></div>
                   </div>
                   <p>
-                    <a href="{{ route('checkout') }}" class="btn btn-master btn-secondary w-100 mt-3">
+                    <a href="{{ route('checkout', 'baru-mulai') }}" class="btn btn-master btn-secondary w-100 mt-3">
                       Start With This Plan
                     </a>
                   </p>
